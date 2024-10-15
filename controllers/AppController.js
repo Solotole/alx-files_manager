8050b65bcbc6efde3f3bebc5805f9cff2d355bc8
Solotole <solomontoleak47@gmail.com>
2024-10-15 15:32:09 +0100

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,13 +1,11 @@
-import dbClient from './../utils/db';
+import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-// const dbClient = require('../utils/db');
-// import redisClient from '../utils/redis';
 
 class AppController {
   static async getStatus(req, res) {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
-           
+
     return res.status(200).json({ redis: redisAlive, db: dbAlive });
   }
 
@@ -15,7 +13,7 @@ class AppController {
   static async getStats(req, res) {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
-                                   
+
     return res.status(200).json({ users: usersCount, files: filesCount });
   }
 }
