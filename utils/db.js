2bf5c5cf663ed2d71ccb9db1e41a1d8723929336
Solotole <solomontoleak47@gmail.com>
2024-10-15 08:57:37 +0100

    mongodb connection

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.connected = false;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, db) => {
+      if (error) {
+	console.error('Error connecting to MongoDB:', error);
+	this.connected = false;
+      } else {
+        this.db = db.db(database);
+	this.connected = true;	
+      }
+    })
+  }
+
+  isAlive() {
+    return this.connected; 
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
