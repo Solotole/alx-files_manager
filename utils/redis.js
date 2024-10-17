90eaa9817f068a9259b1e009b0e9801ed9161f4c
Solotole <solomontoleak47@gmail.com>
2024-10-17 09:56:06 +0100

    redis connect

--- utils/redis.js
+++ utils/redis.js
@@ -1,19 +1,19 @@
 const redis = require('redis');
-// const { promisify } = require('util');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
     this.connected = false;
     this.client = redis.createClient();
-    // this.get = promisify(this.client.get).bind(this);
-    // this.set = promisify(this.client.set).bind(this);
-    // this.del = promisify(this.client.del).bind(this);
+    this.get = promisify(this.client.get).bind(this);
+    this.set = promisify(this.client.set).bind(this);
+    this.del = promisify(this.client.del).bind(this);
     this.client.on('error', (err) => {
       console.log('Redis client error:', err);
     });
-    // this.client.on('connect', () => {
-    // this.connected = true;
-    // });
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
   }
 
   isAlive() {
@@ -21,39 +21,36 @@ class RedisClient {
   }
 
   async get(key) {
-    // return new Promise((resolve, reject) => {
-    // this.client.get(key, (err, value) => {
-    // if (err) {
-    // return reject(err);
-    // }
-    // return resolve(value);
-    // });
-    // });
-    return await this.client.get(key);
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(value);
+      });
+    });
   }
 
   async set(key, value, duration) {
-    // return new Promise((resolve, reject) => {
-    // this.client.set(key, value, 'EX', duration, (err) => {
-    // if (err) {
-    // return reject(err);
-    // }
-    // return resolve();
-    // });
-    // });
-    return await this.client.set(key, value, 'EX', duration);
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
   }
 
   async del(key) {
-    // return new Promise((resolve, reject) => {
-    // this.client.del(key, (err) => {
-    // if (err) {
-    // return reject(err);
-    // }
-    // return resolve();
-    // });
-    // });
-    return await this.client.del(key);
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
   }
 }
 
